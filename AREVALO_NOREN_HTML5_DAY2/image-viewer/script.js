cfe40c02b7e2ba22eb9e3805863e47e7134beef5
24thsaint <admin@blackout.biz.tm>
2017-06-09 11:02:23 +0800

    Day #2 progress

---
+++ AREVALO_NOREN_HTML5_DAY2/image-viewer/script.js
@@ -0,0 +1,40 @@
+function change(file) {
+    const element = document.getElementById('image')
+    element.src = file
+}
+
+function popUp(text) {
+    let textInput = ''
+    const popUp = document.createElement('div')
+    popUp.className = 'fullscreenpopup'
+    const container = document.createElement('div')
+    container.className = 'fullscreenpopup-container'
+    popUp.appendChild(container)
+    const content = document.createElement('div')
+    content.className = 'fullscreenpopup-content'
+    container.appendChild(content)
+    const p1 = document.createElement('p')
+    p1.appendChild(document.createTextNode(text))
+    content.appendChild(p1)
+    const pl1 = document.createElement('p')
+    const input = document.createElement('input')
+    input.setAttribute('type', 'text')
+    input.setAttribute('id', 'location-field')
+    pl1.appendChild(input)
+    content.appendChild(pl1)
+    const p2 = document.createElement('p')
+    content.appendChild(p2)
+    const button = document.createElement('a')
+    button.href = '#'
+    button.className = 'button'
+    button.onclick = () => {
+        const value = document.getElementById('location-field').value
+        change(value)
+        document.body.removeChild(popUp)
+    }
+    p2.appendChild(button)
+    const span = document.createElement('span')
+    span.appendChild(document.createTextNode('OK'))
+    button.appendChild(span)
+    document.body.appendChild(popUp)
+}
