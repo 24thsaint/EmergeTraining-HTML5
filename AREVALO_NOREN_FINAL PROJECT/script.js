a5ebfd4b2ba7bb64f911f319d3482bbd6f941f7b
24thsaint <admin@blackout.biz.tm>
2017-06-11 22:13:30 +0800

    Repositioning and bugfixes

--- AREVALO_NOREN_FINAL PROJECT/script.js
+++ AREVALO_NOREN_FINAL PROJECT/script.js
@@ -32,14 +32,18 @@ class Character {
         this.counter++
     }
 
-    move(x) {
+    move(x, boundaryLeft, boundaryRight) {
+        const pad = 5
+
         if (!this.isDead) {
-            if (this.game.width - this.halfWidth <= (x)) {
-                this.x = this.game.width - this.width
-            } else if (this.halfWidth >= (x)) {
-                this.x = 0
-            } else {
-                this.x = x - (this.halfWidth)
+            this.x = x - boundaryLeft - (this.halfWidth)
+
+            if (this.x <= 0) {
+                this.x = 0 + pad
+            }
+
+            if (x >= boundaryRight - this.halfWidth) {
+                this.x = boundaryRight - boundaryLeft - this.width - pad
             }
         }
     }
@@ -57,10 +61,11 @@ class Bullet {
         this.width = width
         this.height = height
         this.damage = damage
+        this.color = '#00FF00'
     }
 
     render(game) {
-        game.fillStyle = '#00FF00'
+        game.fillStyle = this.color
         game.fillRect(this.x, this.y, this.width, this.height)
         this.y -= 5
         if (this.y <= 0) {
@@ -92,12 +97,14 @@ class Enemy {
         this.health = health
         this.isDestroyed = false
         this.opacity = 1.0
+        this.originalColor = color
         this.color = color
+        this.isBeingHit = false
     }
 
     render(game) {
         if (this.isDestroyed) {
-            // this.game.score.score += 1
+            this.game.score.add(1)
             game.fillStyle = `rgba(255, 255, 255, ${this.opacity})`
             game.fillRect(this.x, this.y, this.width, this.height)
             this.opacity -= 0.1
@@ -110,6 +117,11 @@ class Enemy {
             game.fillRect(this.x, this.y, this.width, this.height)
             this.y += 3
 
+            if (this.isBeingHit) {
+                this.isBeingHit = false
+                this.color = this.originalColor
+            }
+
             if (this.y >= this.game.height) {
                 this.game.removeEntity(this)
             }
@@ -118,6 +130,8 @@ class Enemy {
 
     hit(bullet) {
         this.health -= bullet.damage
+        this.color = bullet.color
+        this.isBeingHit = true
         if (this.health <= 0) {
             this.isDestroyed = true
         }
@@ -125,15 +139,18 @@ class Enemy {
 }
 
 class Game {
-    constructor(game) {
+    constructor(game, score) {
         this.game = game
         this.height = game.canvas.clientHeight
         this.width = game.canvas.clientWidth
         this.gameWindowWidth = 500
         this.maxBoundary = this.width - this.gameWindowWidth
         this.entities = []
-            // this.score = score
+        this.score = score
         this.isGameOver = false
+        this.eventTimer = new Date()
+        this.isSpawningMinions = false
+        this.difficulty = 5
     }
 
     addEntity(entity) {
@@ -157,14 +174,20 @@ class Game {
             })
 
             const chance = Math.floor(Math.random() * 100)
-            if (chance <= 10) {
-                const randomX = Math.floor(Math.random() * (this.width - 50))
-                const enemy = new Enemy(randomX, 0, 50, 50, 1, '#FF0000')
-                this.addEntity(enemy)
-            } else if (chance <= 15) {
-                const randomX = Math.floor(Math.random() * (this.width - 50))
-                const enemy = new Enemy(randomX, 0, 50, 50, 3, '#00FFFF')
-                this.addEntity(enemy)
+            const present = new Date()
+
+            if (PhysicsEngine.getSecondsElapsed(this.eventTimer, present) % this.difficulty === 0 && !this.isSpawningMinions) {
+                this.spawnMinions()
+                this.isSpawningMinions = true
+                setTimeout(() => {
+                    this.isSpawningMinions = false
+                }, 1000)
+                setTimeout(() => {
+                    this.difficulty--
+                        if (this.difficulty <= 0) {
+                            this.difficulty = Math.ceil(Math.random() * 10)
+                        }
+                }, 5000)
             }
         } else {
             this.game.strokeStyle = '#FFFFFF'
@@ -181,12 +204,38 @@ class Game {
         }
     }
 
+    spawnMinions() {
+        const minionSize = 50
+
+        const enemies = []
+
+        for (let i = 0; i < minionSize / 2; i++) {
+            const chance = Math.floor(Math.random() * 100)
+            let enemy
+            if (chance <= 20) {
+                enemy = new Enemy(enemies.length * minionSize, 0, minionSize, minionSize, 1, '#FFF59D')
+            } else if (chance <= 40 && chance > 20) {
+                enemy = new Enemy(enemies.length * minionSize, 0, minionSize, minionSize, 5, '#FF7043')
+            } else if (chance <= 60 && chance > 40) {
+                enemy = new Enemy(enemies.length * minionSize, 0, minionSize, minionSize, 7, '#3949AB')
+            } else if (chance <= 80 && chance > 60) {
+                enemy = new Enemy(enemies.length * minionSize, 0, minionSize, minionSize, 9, '#6A1B9A')
+            } else {
+                enemy = new Enemy(enemies.length * minionSize, 0, minionSize, minionSize, 12, '#B71C1C')
+            }
+            enemies.push(enemy)
+        }
+
+        enemies.forEach((enemy) => {
+            this.addEntity(enemy)
+        })
+    }
+
     restart(character) {
-        // this.score.score = 0
+        this.score.reset()
         this.isGameOver = false
         this.entities = []
         this.addEntity(character)
-            // this.addEntity(this.score)
     }
 
     gameOver() {
@@ -206,7 +255,7 @@ class PhysicsEngine {
 
     static collisionDetected(source, target) {
         const x = PhysicsEngine.collisionAlign(source.x, source.width) - PhysicsEngine.collisionAlign(target.x, target.width)
-        const y = source.y - target.y
+        const y = PhysicsEngine.collisionAlign(source.y, source.height) - PhysicsEngine.collisionAlign(target.y, target.height)
         const radius = (source.width * 0.5) + (target.width * 0.5)
         const distance = Math.sqrt((x * x) + (y * y));
         if (distance < radius) {
@@ -214,29 +263,53 @@ class PhysicsEngine {
         }
         return false
     }
+
+    static generateRandomColor() {
+        const color = Math.floor(Math.random() * 16777215)
+        return color.toString(16)
+    }
+
+    static getSecondsElapsed(past, present) {
+        return Math.ceil((present.getTime() - past.getTime()) / 1000)
+    }
 }
 
 class Score {
-    constructor() {
+    constructor(scoreCanvas) {
         this.score = 0
-            // this.scoreCanvas = scoreCanvas
+        this.scoreCanvas = scoreCanvas
     }
 
     render() {
-        // this.scoreCanvas.fillStyle = '#000000'
-        // this.scoreCanvas.clearRect(0, 0, this.scoreCanvas.canvas.width, this.scoreCanvas.canvas.height)
-        // this.scoreCanvas.strokeStyle = '#000000'
-        // this.scoreCanvas.font = '30px Arial'
-        // this.scoreCanvas.fillText('SCORE: ', 800, 800)
-        // this.scoreCanvas.font = '50px Arial'
-        // this.scoreCanvas.strokeText(`${this.score}`, 800, 800)
-        // console.log('scoreDrawn')
+        this.scoreCanvas.clearRect(0, 0, this.scoreCanvas.canvas.width, this.scoreCanvas.canvas.height)
+
+        this.scoreCanvas.fillStyle = '#999999'
+        this.scoreCanvas.fillRect(0, 0, this.scoreCanvas.canvas.width, this.scoreCanvas.canvas.height)
+
+        this.scoreCanvas.fillStyle = '#FFFFFF'
+        this.scoreCanvas.strokeStyle = '#FFFFFF'
+        this.scoreCanvas.font = '30px Arial'
+        this.scoreCanvas.fillText('SCORE: ', 0, this.scoreCanvas.canvas.clientHeight - 10)
+        this.scoreCanvas.font = '30px Arial'
+        this.scoreCanvas.strokeText(`${this.score}`, 120, this.scoreCanvas.canvas.clientHeight - 10)
+    }
+
+    add(score) {
+        this.score += score
+        this.render()
+    }
+
+    reset() {
+        this.score = 0
+        this.render()
     }
 }
 
 window.onload = () => {
     const handler = () => {
         window.removeEventListener('click', handler)
+        const starter = document.getElementById('starter')
+        starter.remove()
         initialize()
     }
     window.addEventListener('click', handler, false)
@@ -247,6 +320,14 @@ function initialize() {
     gameElement.width = 500
     gameElement.height = window.innerHeight
     const gameCanvas = gameElement.getContext('2d')
+    const boundary = gameElement.getBoundingClientRect()
+
+    const scoreElement = document.getElementById('score')
+    scoreElement.width = gameElement.width
+    scoreElement.height = 50
+    const scoreCanvas = scoreElement.getContext('2d')
+    const score = new Score(scoreCanvas)
+    score.render()
 
     const entitySize = 50
     const entityMargin = entitySize * 2
@@ -256,11 +337,11 @@ function initialize() {
     gameCanvas.clearRect(0, 0, width, height)
 
     const character = new Character(PhysicsEngine.centerize(width, entitySize), height - entityMargin, entitySize)
-    const game = new Game(gameCanvas)
+    const game = new Game(gameCanvas, score)
     game.addEntity(character)
 
     gameElement.onmousemove = (event) => {
-        character.move(event.x)
+        character.move(event.x, boundary.left, boundary.right)
     }
 
     gameElement.onclick = () => {
